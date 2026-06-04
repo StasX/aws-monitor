5a89e37b5714e2964757cd8305239a578146d05c
StasX <s.mestechkin@gmail.com>
2026-06-04 20:22:38 +0300

    Fix security issue

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def home():
     return render_template_string(html_template, instance_data=instance_data, vpc_data=vpc_data, lb_data=lb_data, ami_data=ami_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
